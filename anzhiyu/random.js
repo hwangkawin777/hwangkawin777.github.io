95ca8f0bb968ccb1a26deda88814bd94fbed017f
k mj <3698647820@qq.com>
2025-12-26 01:08:49 +0800

    Site updated: 2025-12-26 01:08:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/24/hello-world/","2025/12/24/html/"];function toRandomPost(){
+var posts=["2025/12/24/html/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
