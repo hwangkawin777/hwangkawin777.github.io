66acc300d6bd48df30b2ad837c9c88db2e1de795
k mj <3698647820@qq.com>
2025-12-27 19:52:59 +0800

    Site updated: 2025-12-27 19:52:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/24/html/"];function toRandomPost(){
+var posts=["2025/12/27/小兔鲜项目中学到的细节知识/","2025/12/24/html/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
