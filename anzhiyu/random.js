6adae12226cf517acba3671dafac7721ecdc7b47
k mj <3698647820@qq.com>
2026-06-10 16:30:08 +0800

    Site updated: 2026-06-10 16:30:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/27/小兔鲜项目中学到的细节知识/","2026/02/13/滚动猫咪中的细节/","2025/12/24/html/","2026/06/09/行人重识别-论文/"];function toRandomPost(){
+var posts=["2026/02/13/滚动猫咪中的细节/","2025/12/24/html/","2026/06/09/行人重识别-论文/","2025/12/27/小兔鲜项目中学到的细节知识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
